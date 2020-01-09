42bb330b1442069d4816b7a8927b187da90939bd
nomizo-iox <nomizo.iox@gmail.com>
2020-01-08 22:02:39 -0600

    first commit

---
+++ Chapter3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chapter3
+//
+//  Created by Samuel F. Ademola on 1/8/20.
+//  Copyright © 2020 Nomizo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Swift-Chapter3
