9bc6541a0bf6493319b7ab7a47db13f95a3c42a0
nomizo-iox <nomizo.iox@gmail.com>
2020-01-09 00:53:07 -0600

    Created 4 buttons with 1 IBAction.

--- Chapter3/ViewController.swift
+++ Chapter3/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showMessage(sender:  UIButton) {
+        
+        // Creating a dictionary for the emoji
+        let emojiDict = ["😱": "Shock", "😤": "Fumming", "🤔": "Interesting", "🙄": "Obviously" ]
+        
+        // Sender Button
+        let selectdButton = sender
+        
+        if let wordToLookup = selectdButton.titleLabel?.text {
+            
+            let meaning = emojiDict[wordToLookup]
+          
+            
+            let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            
+            present(alertController, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
 }
 
